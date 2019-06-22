2ed4b189f853341589967699cdfbec8a96566d59
Tung <tung@bodidata.com>
2019-06-23 00:02:06 +0700

    test: this actual breaks

--- tests/long_string.spec.js
+++ tests/long_string.spec.js
@@ -3,13 +3,36 @@ const { parse } = require("..");
 const query = /* cypher */ `
   MATCH (n)
 
-  // Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-  // Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-  // Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-  // Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-  // Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-  // Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-  // Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+  CREATE (a1:LONG_LABEL)-[:LONG_RELATIONSHIP_NAME]->(b1:LONGER_LABEL { long_key: 'long_value'})
+  CREATE (a2:LONG_LABEL)-[:LONG_RELATIONSHIP_NAME]->(b2:LONGER_LABEL { long_key: 'long_value'})
+  CREATE (a3:LONG_LABEL)-[:LONG_RELATIONSHIP_NAME]->(b3:LONGER_LABEL { long_key: 'long_value'})
+  CREATE (a4:LONG_LABEL)-[:LONG_RELATIONSHIP_NAME]->(b4:LONGER_LABEL { long_key: 'long_value'})
+  CREATE (a5:LONG_LABEL)-[:LONG_RELATIONSHIP_NAME]->(b5:LONGER_LABEL { long_key: 'long_value'})
+  CREATE (a6:LONG_LABEL)-[:LONG_RELATIONSHIP_NAME]->(b6:LONGER_LABEL { long_key: 'long_value'})
+  CREATE (a7:LONG_LABEL)-[:LONG_RELATIONSHIP_NAME]->(b7:LONGER_LABEL { long_key: 'long_value'})
+  CREATE (a8:LONG_LABEL)-[:LONG_RELATIONSHIP_NAME]->(b8:LONGER_LABEL { long_key: 'long_value'})
+  CREATE (a9:LONG_LABEL)-[:LONG_RELATIONSHIP_NAME]->(b9:LONGER_LABEL { long_key: 'long_value'})
+  CREATE (a10:LONG_LABEL)-[:LONG_RELATIONSHIP_NAME]->(b10:LONGER_LABEL { long_key: 'long_value'})
+  CREATE (a11:LONG_LABEL)-[:LONG_RELATIONSHIP_NAME]->(b11:LONGER_LABEL { long_key: 'long_value'})
+  CREATE (a12:LONG_LABEL)-[:LONG_RELATIONSHIP_NAME]->(b12:LONGER_LABEL { long_key: 'long_value'})
+  CREATE (a13:LONG_LABEL)-[:LONG_RELATIONSHIP_NAME]->(b13:LONGER_LABEL { long_key: 'long_value'})
+  CREATE (a14:LONG_LABEL)-[:LONG_RELATIONSHIP_NAME]->(b14:LONGER_LABEL { long_key: 'long_value'})
+  CREATE (a15:LONG_LABEL)-[:LONG_RELATIONSHIP_NAME]->(b15:LONGER_LABEL { long_key: 'long_value'})
+  CREATE (a16:LONG_LABEL)-[:LONG_RELATIONSHIP_NAME]->(b16:LONGER_LABEL { long_key: 'long_value'})
+  CREATE (a17:LONG_LABEL)-[:LONG_RELATIONSHIP_NAME]->(b17:LONGER_LABEL { long_key: 'long_value'})
+  CREATE (a18:LONG_LABEL)-[:LONG_RELATIONSHIP_NAME]->(b18:LONGER_LABEL { long_key: 'long_value'})
+  CREATE (a19:LONG_LABEL)-[:LONG_RELATIONSHIP_NAME]->(b19:LONGER_LABEL { long_key: 'long_value'})
+  CREATE (a20:LONG_LABEL)-[:LONG_RELATIONSHIP_NAME]->(b20:LONGER_LABEL { long_key: 'long_value'})
+  CREATE (a21:LONG_LABEL)-[:LONG_RELATIONSHIP_NAME]->(b21:LONGER_LABEL { long_key: 'long_value'})
+  CREATE (a22:LONG_LABEL)-[:LONG_RELATIONSHIP_NAME]->(b22:LONGER_LABEL { long_key: 'long_value'})
+  CREATE (a23:LONG_LABEL)-[:LONG_RELATIONSHIP_NAME]->(b23:LONGER_LABEL { long_key: 'long_value'})
+  CREATE (a24:LONG_LABEL)-[:LONG_RELATIONSHIP_NAME]->(b24:LONGER_LABEL { long_key: 'long_value'})
+  CREATE (a25:LONG_LABEL)-[:LONG_RELATIONSHIP_NAME]->(b25:LONGER_LABEL { long_key: 'long_value'})
+  CREATE (a26:LONG_LABEL)-[:LONG_RELATIONSHIP_NAME]->(b26:LONGER_LABEL { long_key: 'long_value'})
+  CREATE (a27:LONG_LABEL)-[:LONG_RELATIONSHIP_NAME]->(b27:LONGER_LABEL { long_key: 'long_value'})
+  CREATE (a28:LONG_LABEL)-[:LONG_RELATIONSHIP_NAME]->(b28:LONGER_LABEL { long_key: 'long_value'})
+  CREATE (a29:LONG_LABEL)-[:LONG_RELATIONSHIP_NAME]->(b29:LONGER_LABEL { long_key: 'long_value'})
+  CREATE (a30:LONG_LABEL)-[:LONG_RELATIONSHIP_NAME]->(b30:LONGER_LABEL { long_key: 'long_value'})
 
   RETURN n;
   `;
