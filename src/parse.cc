5dfa0699bdfb2ba41f8792df8d8121289c77f6d3
Tung <tung@bodidata.com>
2019-06-22 23:55:34 +0700

    fix: use fparse for long string

--- src/parse.cc
+++ src/parse.cc
@@ -7,42 +7,79 @@
 using namespace std;
 using namespace Napi;
 
+cypher_parse_result_t *ParseLongQuery(string query)
+{
+  string temp_file_name = std::tmpnam(nullptr);
+  auto file_name = temp_file_name.c_str();
+
+  FILE *inputStream = fopen(file_name, "w");
+  auto str = query.c_str();
+  for (unsigned int i = 0; i < query.length(); ++i)
+  {
+    fputc(str[i], inputStream);
+  }
+  fclose(inputStream);
+
+  inputStream = fopen(file_name, "r");
+
+  auto config = cypher_parser_new_config();
+  cypher_parse_result_t *result = cypher_fparse(inputStream, NULL, config, CYPHER_PARSE_ONLY_STATEMENTS);
+  cypher_parser_config_free(config);
+  remove(file_name);
+
+  return result;
+}
+
+cypher_parse_result_t *ParseShortQuery(string query)
+{
+  auto config = cypher_parser_new_config();
+  cypher_parse_result_t *result = cypher_parse(query.c_str(), NULL, config, CYPHER_PARSE_ONLY_STATEMENTS);
+  cypher_parser_config_free(config);
+  return result;
+}
+
+cypher_parse_result_t *Parse(string query)
+{
+  if (query.length() >= 1024)
+  {
+    return ParseLongQuery(query);
+  }
+  else
+  {
+    return ParseShortQuery(query);
+  }
+}
+
 Object ParseQuery(const CallbackInfo &info)
 {
   Env env = info.Env();
   Object obj = Object::New(env);
 
   // parsing
-  auto query = string(info[0].As<String>());
+  string query = string(info[0].As<String>());
+  cypher_parse_result_t *result = Parse(query);
 
-  cout << query << endl;
-
-  auto str = query.c_str();
-
-  auto rootObj = Object::New(env);
-
-  
-  auto config = cypher_parser_new_config();
-  cypher_parse_result_t *result = cypher_parse(str, NULL, config, CYPHER_PARSE_ONLY_STATEMENTS);
-
-  if (result == NULL) {
-    throw Error::New(env, "Cypher Parse Exception");    
+  if (result == NULL)
+  {
+    throw Error::New(env, "Cypher Parse Exception");
   }
 
   auto nErrors = cypher_parse_result_nerrors(result);
   auto errors = Array::New(env, nErrors);
 
-  for (unsigned int i = 0; i < nErrors; i++) {
+  for (unsigned int i = 0; i < nErrors; i++)
+  {
     auto node = cypher_parse_result_get_error(result, i);
     if (!node)
       continue;
 
     auto error = Object::New(env);
 
+    cerr << "error[" << i << "]: " << cypher_parse_error_message(node) << endl;
+
     error.Set(String::New(env, "message"), String::New(env, cypher_parse_error_message(node)));
     error.Set(String::New(env, "context"), String::New(env, cypher_parse_error_context(node)));
     error.Set(String::New(env, "contextOffset"), Number::New(env, cypher_parse_error_context_offset(node)));
-
 
     auto position = cypher_parse_error_position(node);
     auto jsPosition = Object::New(env);
@@ -50,28 +87,34 @@ Object ParseQuery(const CallbackInfo &info)
     jsPosition.Set(String::New(env, "column"), Number::New(env, position.column));
     jsPosition.Set(String::New(env, "offset"), Number::New(env, position.offset));
 
-    error.Set(String::New(env, "posotion"), jsPosition);
+    error.Set(String::New(env, "position"), jsPosition);
 
-    errors[i] = error; 
+    errors[i] = error;
   }
-  
+
   // constructing response
   obj.Set(String::New(env, "nnodes"), cypher_parse_result_nnodes(result));
-  obj.Set(String::New(env, "eof"), cypher_parse_result_eof(result));
+  obj.Set(String::New(env, "eof"), cypher_parse_result_eof(result) == 1);
   obj.Set(String::New(env, "errors"), errors);
 
   // constructing root node walker
   auto root = cypher_parse_result_get_root(result, 0);
-  ASTNode node(root, rootObj);
-
-  // recursively walk from root
-  node.Walk(env);
+  if (root == NULL)
+  {
+    obj.Set(String::New(env, "root"), env.Null());
+  }
+  else
+  {
+    auto rootObj = Object::New(env);
+    ASTNode node(root, rootObj);
+    // recursively walk from root
+    node.Walk(env);
+    obj.Set(String::New(env, "root"), rootObj);
+  }
 
   // clean up
   cypher_parse_result_free(result);
-  cypher_parser_config_free(config);
 
-  obj.Set(String::New(env, "root"), rootObj);
   return obj;
 }
 
